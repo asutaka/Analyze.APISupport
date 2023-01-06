9b56255d9c41d53698dd915f11632e4b691537c1
Nguyễn Văn Phú <phunv@ftech.ai>
2023-01-06 18:33:39 +0700

    big update

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
+const tele = require('./teleapi');
 const express = require('express')
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 8000
 
-let routes = require('./api/routes') //importing route
-routes(app);
-
-let cronjob = require('./cronjob').telegram();
-
+tele.connect();
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 })
-
 app.listen(port)
 
 console.log('RESTful API server started on: ' + port)
+
+app.post('/telegram', async (req, res)  => {
+    let data = req.body;
+    try
+    {
+        tele.sendMessage(data.phone, data.message);
+    }
+    catch(error)
+    {
+        console.log("TelegramController|ERROR(SendMessage)|",data.phone, data.message, error);
+    }
+    return res.message(200).json({ success: true });
+})
 
 
--- teleapi.js
+++ teleapi.js
@@ -1,68 +1,47 @@
 const { Api, TelegramClient } = require('telegram')
 const { StringSession } = require('telegram/sessions')
-const input = require('input') // npm i input
-const db = require('./api/db')
 
 const apiId = 5128731
 const apiHash = 'a894a079cfeadf0bd0646f53bf2587c6'
 const stringSession = new StringSession('1BQANOTEuMTA4LjU2LjE1NAG7bR02Q/sxF2vFjd+p3lc78mlYedXpZhXilnKkZW6Wc/fl5QtPL4LYRResy5rdPixE497sy5qkhhC0wCqtg4HzLqRPx3iCFsKc+DiFJ3fA+/pJMrdH4LetBZqkCymEFOlkMp6Qbu1Pz/z4zMVw/IHt2oZkezEEcbKCuvpEP5ZB99G+Z8HwnLZ4fLbh+He+Z1t7L/Gag+wYppwcRjQDlonBCdxWRVkyoe8EjnVo1o0sgsz6ai4EQ8UeH5Kq/2IPkRzdqO1qpI9yRBEMpWV8D1kH+F9vXDpdehArh8eIGRlvOCZY1OwXFnrmROxIc30c9GwAX/yiuFUeQJAau/4hICFZzQ=='); // fill this later with the value from session.save()
 const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 });
 
+let arrUser = [];
+
 module.exports = {
 	connect: async () => {
 		await client.connect(); // This assumes you have already authenticated with .start()
 	},
 	sendMessage: async (_phone, _message) => {
-		storeGet(_phone, async (telId) => {
-			if(telId <= 0)
-			{
-				const result = await client.invoke(
-					new Api.contacts.ImportContacts({
-					  contacts: [
-						new Api.InputPhoneContact({
-						  clientId: 12345678,
-						  phone: _phone,
-						  firstName: "",
-						  lastName: "",
-						}),
-					  ],
-					})
-				);
-				
-				if(result.users.length > 0){
-					telId = result.users[0].id.value;
-					//Store
-					let sqlInsert = 'INSERT INTO TelegramUser VALUES (?, ?)'
-					db.query(sqlInsert, [telId, _phone], (err, response) => {
-						if (err)
-						{
-							console.log("TeleAPI|ERROR(Store TelegramUser)| %o", err)
-							return;
-						} 
-					})
-				}
-			}
-
-			if(telId > 0){
-				await client.sendMessage(telId, { message: _message });
-			}
-		});
-	}
-}
-
-function storeGet(phone, callback)
-{
-	let sql = 'SELECT * FROM TelegramUser WHERE phone = ?'
-	db.query(sql, [phone], (err, response) => {
-			if (err)
-			{
-				console.log("TeleAPI|ERROR(Get TelegramUser)| %o", err)
-				return callback(0);
-			} 
-			if(response.length > 0){
-				return callback(response[0].id);
-			}
+		var lEntity = arrUser.filter(x => x.phone == _phone);
+		var telId = 0;
+		if(lEntity.length > 0)
+		{
+			telId = lEntity[0].telId;
+		}
+		else
+		{
+			const result = await client.invoke(
+				new Api.contacts.ImportContacts({
+				  contacts: [
+					new Api.InputPhoneContact({
+					  clientId: 12345678,
+					  phone: _phone,
+					  firstName: "",
+					  lastName: "",
+					}),
+				  ],
+				})
+			);
 			
-			return callback(0); 
-		});
+			if(result.users.length > 0){
+				telId = result.users[0].id.value;
+				arrUser.push({ phone: _phone, telId: telId });
+			}
+		}
+
+		if(telId > 0){
+			await client.sendMessage(telId, { message: _message });
+		}
+	}
 }
